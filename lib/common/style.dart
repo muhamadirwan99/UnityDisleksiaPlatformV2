2254a13a70499637a9ae70fb8e2d40560c97e44e
Arsenius Purbandono <xzars@upi.edu>
2023-04-20 11:25:12 +0700

    set up style and config

---
+++ lib/common/style.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Text Styles
+
+//body
+final TextStyle bodySmall = GoogleFonts.roboto(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
+final TextStyle bodyMedium = GoogleFonts.roboto(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+);
+final TextStyle bodyLarge = GoogleFonts.roboto(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
+
+//label
+final TextStyle labelSmall = GoogleFonts.roboto(
+  fontSize: 11,
+  fontWeight: FontWeight.w500,
+);
+final TextStyle labelMedium = GoogleFonts.roboto(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+final TextStyle labelLarge = GoogleFonts.roboto(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+//title
+final TextStyle titleSmall = GoogleFonts.roboto(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+final TextStyle titleMedium = GoogleFonts.roboto(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+final TextStyle titleLarge = GoogleFonts.roboto(
+  fontSize: 22,
+  fontWeight: FontWeight.w500,
+);
+
+//headline
+final TextStyle headlineSmall = GoogleFonts.roboto(
+  fontSize: 24,
+  fontWeight: FontWeight.w400,
+);
+final TextStyle headlineMedium = GoogleFonts.roboto(
+  fontSize: 28,
+  fontWeight: FontWeight.w400,
+);
+final TextStyle headlineLarge = GoogleFonts.roboto(
+  fontSize: 32,
+  fontWeight: FontWeight.w400,
+);
+
+//Display
+final TextStyle displaySmall = GoogleFonts.roboto(
+  fontSize: 36,
+  fontWeight: FontWeight.w400,
+);
+final TextStyle displayMedium = GoogleFonts.roboto(
+  fontSize: 45,
+  fontWeight: FontWeight.w400,
+);
+final TextStyle displayLarge = GoogleFonts.roboto(
+  fontSize: 57,
+  fontWeight: FontWeight.w400,
+);
+
+// red Pallete
+const Color red100 = Color(0xffFDE6D3);
+const Color red200 = Color(0xffFCC7A8);
+const Color red300 = Color(0xffF8A07B);
+const Color red400 = Color(0xffF17A59);
+const Color red500 = Color(0xffE84126);
+const Color red600 = Color(0xffC7261B);
+const Color red700 = Color(0xffA71315);
+const Color red800 = Color(0xff860C17);
+const Color red900 = Color(0xff6F0719);
+
+// blue Pallete
+const Color blue100 = Color(0xffC8F9EC);
+const Color blue200 = Color(0xff93F4E3);
+const Color blue300 = Color(0xff59DED2);
+const Color blue400 = Color(0xff30BEBD);
+const Color blue500 = Color(0xff018893);
+const Color blue600 = Color(0xff006A7E);
+const Color blue700 = Color(0xff005069);
+const Color blue800 = Color(0xff003955);
+const Color blue900 = Color(0xff002A46);
+
+// yellow Pallete
+const Color yellow100 = Color(0xffFFF7D7);
+const Color yellow200 = Color(0xffFFEDB0);
+const Color yellow300 = Color(0xffFFE089);
+const Color yellow400 = Color(0xffFFD36C);
+const Color yellow500 = Color(0xffFFBE3B);
+const Color yellow600 = Color(0xffDB9A2B);
+const Color yellow700 = Color(0xffB77A1D);
+const Color yellow800 = Color(0xff935B12);
+const Color yellow900 = Color(0xff7A460B);
+
+// gray Pallete
+const Color gray100 = Color(0xffFAFAFA);
+const Color gray200 = Color(0xffDCDCDC);
+const Color gray300 = Color(0xffC9C9C9);
+const Color gray400 = Color(0xffAEAEAE);
+const Color gray500 = Color(0xff8E8E8E);
+const Color gray600 = Color(0xff696969);
+const Color gray700 = Color(0xff575757);
+const Color gray800 = Color(0xff393939);
+const Color gray900 = Color(0xff1B1B1B);
+
+class dimension {
+  static double thumbWebinarSize = 110;
+  static double thumbVideoWidth = 166;
+  static double thumbVideoHeight = 110;
+  static double bannerWidth = 380;
+  static double bannerHeight = 180;
+  static double mediaPlayerWidth = 380;
+  static double mediaPlayerHeight = 214;
+}
